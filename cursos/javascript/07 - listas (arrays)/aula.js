dfe68cb5f1ca971b74dfa00d2b7f6efe02772e2e
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2022-11-05 14:26:01 -0300

    criei aula - arrays

---
+++ cursos/javascript/07 - listas (arrays)/aula.js
@@ -0,0 +1,17 @@
+const notas = [];
+let soma = 0;
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+notas.push(8);
+
+for(let i = 0; i < notas.length; i++) {
+    soma = soma + notas[i];
+}
+
+const media = soma / notas.length;
+
+console.log(media);
