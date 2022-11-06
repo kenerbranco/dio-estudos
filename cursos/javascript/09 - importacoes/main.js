e1e032a08319d61e26e4a35245ca4f0f7369f730
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2022-11-06 13:42:36 -0300

    finalizei aula importacao/exportacao nodejs

--- cursos/javascript/09 - importacoes/main.js
+++ cursos/javascript/09 - importacoes/main.js
@@ -1,3 +1,24 @@
+/*
+    Uma sala contem 5 alunos e para cada aluno foi sorteado um número de 1 - 100.
+    Faça um programa que receba os 5 números sorteados para os alunos e mostre o maior número sorteado
+
+    Dados de entrada:
+    5, 50, 10, 98, 23
+
+    Sáida:
+    98
+*/
+
 const { gets, print } = require('./funcoes-auxiliares');
 
-print(gets());
+const quantidadeDeAlunos = gets();
+let maiorNumeroEncontrado = 0;
+
+for (let i = 0; i < quantidadeDeAlunos; i++) {
+    const numeroSorteado = gets();
+    if (numeroSorteado > maiorNumeroEncontrado) {
+        maiorNumeroEncontrado = numeroSorteado;
+    }
+}
+
+print(maiorNumeroEncontrado);
