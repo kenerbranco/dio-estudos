8da25a2679822d632914515cb964b4d04bb59ab4
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2022-11-10 05:53:22 -0300

    criei pokemon-model.js

--- bootcamps/orange-tech+/projeto-pokedex/assets/js/main.js
+++ bootcamps/orange-tech+/projeto-pokedex/assets/js/main.js
@@ -1,18 +1,20 @@
 
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${type}</li>`)
+}
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poision</li>
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
                 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
     
@@ -23,9 +25,9 @@ const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
     
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('')
     // .map() = msm função do for, percorre todos results e adiciona numa lista
     // .map(value, index, array) = parâmetros da função
     // .map((pokemon) => convertPokemonToLi(pokemon)) == .map(convertPokemonToLi)
-    
+
 })
--- bootcamps/orange-tech+/projeto-pokedex/assets/js/poke-api.js
+++ bootcamps/orange-tech+/projeto-pokedex/assets/js/poke-api.js
@@ -1,9 +1,33 @@
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types  // Primeira posição array
+    
+    pokemon.types = types
+    pokemon.type = type
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then(convertPokeApiDetailToPokemon)
+}
+
 pokeApi.getPokemons = (offset=0, limit=10) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.log(error))
+
+    return fetch(url)  // Requisição na API
+        .then((response) => response.json())  // Converte para json
+        .then((jsonBody) => jsonBody.results)  // Lista dos pokemons até limit
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) // Transf. lista p/ detail
+        .then((detailRequests) => Promise.all(detailRequests))  // Faz e espera todas req.
+        .then((pokemonDetails) => pokemonDetails)
 }
