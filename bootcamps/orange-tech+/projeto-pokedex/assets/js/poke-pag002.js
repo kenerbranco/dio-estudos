2c37099aed8dc4cfddebaf2c8e83075b451c7b10
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2022-11-16 19:20:43 -0300

    testes extração dados para pag002

--- bootcamps/orange-tech+/projeto-pokedex/assets/js/poke-pag002.js
+++ bootcamps/orange-tech+/projeto-pokedex/assets/js/poke-pag002.js
@@ -1,13 +1,3 @@
-function clickPokemon(id, name, typeOne, typeTwo = '', photo) {
-
-    const pokemonNumber = id
-    const pokemonName = name
-    const pokemonTypeOne = typeOne
-    const pokemonTypeTwo = typeTwo
-    const pokemonPhoto = photo
-
-}
-
 function loadPage() {
 
     const pokemonHtml = document.getElementById('pokemonAbout')
---
+++ bootcamps/orange-tech+/projeto-pokedex/assets/js/pokemon-model-pag002.js
@@ -0,0 +1,10 @@
+function clickPokemon(id, name, typeOne, typeTwo = '', photo) {
+
+    const pokemonNumber = id
+    const pokemonName = name
+    const pokemonTypeOne = typeOne
+    const pokemonTypeTwo = typeTwo
+    const pokemonPhoto = photo
+
+}
+
