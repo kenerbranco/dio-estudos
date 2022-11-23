dd1e4c6673fe0e134fb1e92519813e74cda2de58
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2022-11-23 04:47:26 -0300

    iniciado navegação do projeto

---
+++ bootcamps/orange-tech+/biblioteca-react/aulas/01-iniciando-biblioteca-react/src/App.js
@@ -0,0 +1,14 @@
+import Button from './components/Button'
+
+function App() {
+  return ( 
+
+  <div className="App">
+    <h1>Olá React</h1>
+    <Button title="Entrar"/>
+  </div>
+
+  );
+}
+
+export default App;
