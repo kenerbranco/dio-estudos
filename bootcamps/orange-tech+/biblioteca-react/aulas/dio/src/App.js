872f169a924838f0134ee0afac7e2586247ebef9
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2022-11-23 00:04:20 -0300

    criado primeiro componente em react

--- bootcamps/orange-tech+/biblioteca-react/aulas/dio/src/App.js
+++ bootcamps/orange-tech+/biblioteca-react/aulas/dio/src/App.js
@@ -1,8 +1,13 @@
+import Button from './components/Button'
+
 function App() {
-  return (
-    <div className="App">
-      Olá React
-    </div>
+  return ( 
+
+  <div className="App">
+    <h1>Olá React</h1>
+    <Button title="Entrar"/>
+  </div>
+
   );
 }
 
